156eb54f4ad4666a122a8c6095320be5215105af
sbpirzada <159937919+sbpirzada@users.noreply.github.com>
2024-02-26 12:19:43 +0500

    This is my project of Hello World.

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+let message = "Hello World";
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
---
+++ main.ts
@@ -0,0 +1,14 @@
+let message = "Hello World";
+
+
+
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");                 
+console.log("Hello, World!");
+console.log("Hello, World!");
